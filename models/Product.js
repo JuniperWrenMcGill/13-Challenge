a4c997a86fada6b5cc9adb57f6975a96b255f2cf
Juniper <juniperwrenarts@gmail.com>
2024-02-11 11:18:11 -0500

    debugged connection and models - now able to seed

--- models/Product.js
+++ models/Product.js
@@ -2,9 +2,11 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+// Import the Category model
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
@@ -38,7 +40,8 @@ Product.init(
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'Category', 
+        model: Category, // Reference the Category model
+        key: 'id',      // Reference the id column in the Category model
       },
     },
   },
